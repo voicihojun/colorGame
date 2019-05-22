44d2428ef45a7268a47a257828f140c2d7b83181
voicihojun <voicihojun@gmail.com>
2019-05-22 19:54:49 -0400

    second commit, all functionality is implemented.

--- color_game.js
+++ color_game.js
@@ -1,6 +1,12 @@
+// <li> easy, hard, newcolors need to be changed by <button>
+// when we use easy mode and we search the right answer, the squares' number need to be 3. 
+// just one line. 
+// and in using transition, disappering the square take more time like fading out. 
+// the other functions are implemented!. 
+
 
 var squares = document.querySelectorAll('.square');
-var rgb, rgbEach;
+var rgb;
 
 function makeRgb() {
 	var r,g,b;
@@ -12,13 +18,17 @@ function makeRgb() {
 }
 
 function makeNewColors() {
+	if(document.getElementById("newColors").textContent == "PLAY AGAIN?") {
+		document.getElementById("newColors").textContent = "NEW COLORS";
+	}
+	document.getElementsByTagName("h3")[0].style.backgroundColor = "skyblue";
+	document.getElementById("message").innerHTML = "";
 	rgb = makeRgb();
 	rgbEach = rgb.slice(4, -1).split(", ");
-	var randomNum = Math.floor(Math.random() * 6);
+	var randomNum = Math.floor(Math.random() * squares.length);
 	console.log("random number is : " + randomNum);
 	for(var i=0; i < squares.length; i++) {	
 		if(i == randomNum) {
-
 			squares[i].style.backgroundColor = rgb;	
 			console.log("real rgb is " + rgb);
 		} else {
@@ -39,18 +49,23 @@ function makeNewColors() {
 }	
 
 function makeItEasy() {	
-	var line = document.getElementsByClassName("line")
-	for(var i=0; i < line.length; i++) {
-		line[i].style.visibility = "hidden";
+	var line2 = document.getElementsByClassName("line2")
+	for(var i=0; i < line2.length; i++) {
+		line2[i].style.visibility = "hidden";
 	}
+
+	squares = document.querySelectorAll('.line1');
+	makeNewColors();
 }
 
 function makeItHard() {	
-	var line = document.getElementsByClassName("line")
-	console.log(line);
-	for(var i=0; i < line.length; i++) {
-		line[i].style.visibility = "visible";
+	var line2 = document.getElementsByClassName("line2")
+	console.log(line2);
+	for(var i=0; i < line2.length; i++) {
+		line2[i].style.visibility = "visible";
 	}
+	squares = document.querySelectorAll('.square');
+	makeNewColors();
 }
 
 window.onload = makeNewColors();
@@ -59,8 +74,7 @@ document.getElementById("easy").addEventListener("click", makeItEasy);
 document.getElementById("hard").addEventListener("click", makeItHard);
 
 
-function search() {
-	
+function search() {	
 	console.log(rgb);
 	var bgRgb = this.style.backgroundColor;
 	console.log(bgRgb);
@@ -68,9 +82,13 @@ function search() {
 		for(var i=0; i < squares.length; i++){
 			squares[i].style.visibility = "visible";	
 			squares[i].style.backgroundColor = rgb;	
+			document.getElementById("message").innerHTML = "CORRECT!";
+			document.querySelector("h3").style.backgroundColor = rgb;
+			document.querySelector("#newColors").textContent = "PLAY AGAIN?";
 		} 
 	} else {
 		this.style.visibility = "hidden";
+		document.getElementById("message").innerHTML = "TRY AGAIN?";
 	}	
 }
 
